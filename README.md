0312f416ad7422aa321c81a3ddc280672217f0a0
guipatriota <guipatriota@gmail.com>
2020-08-07 02:10:26 -0300

    mvp v1

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # TaskManager-Flask-Test
 Simple web task list manager in pyhton3 with Flask
 
-TO RUN - at terminal:
-$ python3 app.py
+## TO RUN - at terminal:
+1 - Activate env:
+$ source env/bin/activate
+2 - Run app.py
+$ python app.py
+
+## TO CREATE NEW DB - at terminal:
+1 - Activate env:
+$ source env/bin/activate
+2 - Run python:
+$ python
+3 - Create db:
+$> from app import db
+$> db.create_all()
+$> exit() 
--- app.py
+++ app.py
@@ -1,10 +1,73 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import pytz
+from pytz import timezone
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+user_timezone = pytz.timezone('America/Sao_Paulo') #UTC-3
+#user_timezone = pytz.timezone('America/Los_Angeles') #UTC-7
+utc_timezone = pytz.utc
 
-@app.route('/')
+class Todo(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	content = db.Column(db.String(200), nullable=False)
+	completed = db.Column(db.Integer, default=0)
+	date_created = db.Column(db.DateTime(timezone=True), default=datetime.now(tz=pytz.utc))
+
+	def __repr__(self):
+		return '<Task %r>' % self.id
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-	return render_template('index.html')
+	if request.method == 'POST':
+		task_content = request.form['content']
+		new_task = Todo(content=task_content)
+
+		try:
+			db.session.add(new_task)
+			db.session.commit()
+			return redirect('/')
+		except:
+			return 'There was an issue adding your task'
+
+	else:
+		tasks = Todo.query.order_by(Todo.date_created).all()
+		# print(tasks)
+		# print(tasks[0].date_created)
+		# print(tasks[0].date_created.replace(tzinfo=utc_timezone))
+		# print(tasks[0].date_created.replace(tzinfo=utc_timezone).astimezone(user_timezone))
+		return render_template('index.html', tasks=tasks, user_timezone=user_timezone, utc_timezone=utc_timezone)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+	task_to_delete = Todo.query.get_or_404(id)
+
+	try:
+		db.session.delete(task_to_delete)
+		db.session.commit()
+		return redirect('/')
+	except:
+		return 'There was a problem deleting that task.'
+	
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+	task = Todo.query.get_or_404(id)
+
+	if request.method == 'POST':
+		task.content = request.form['content']
+
+		try:
+			db.session.commit()
+			return redirect('/')
+		except:
+			return 'There was a problem updating this task.'
+
+	else:		
+		return render_template('update.html', task=task)
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
